c05c9e7e925cb65900ecb50a148490b888ee93cf
Dylan Palmboom <nofxboy1234@gmail.com>
2023-08-18 12:29:31 +0200

    Associate users and posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true
+
+  has_many :posts
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_18_100221) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_18_101048) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "link"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
